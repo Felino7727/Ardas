7af270815f1895e11722a8b3fa11af111e81fd99
Felino7727 <antontsev.nikita@gmail.com>
2018-08-15 17:19:04 +0300

    15.08.2018

--- src/app/tasks-correct/tasks-correct.component.ts
+++ src/app/tasks-correct/tasks-correct.component.ts
@@ -14,17 +14,33 @@ export class TasksCorrectComponent implements OnInit {
   @Input()
   id: any;
   task: TasksModels[] = [];
+  name;
+  creation_date;
+  due_date;
+  physical_progress;
+  obj_status;
+  description;
+  tags;
 
   constructor(private router: ActivatedRoute, private taskService: TasksServices) {
   }
-  Onclick(){console.log(1)}
+  Onclick():void{
+    console.log(this.name);
+  }
   ngOnInit() {
     this.id = this.router.snapshot.params['id'];
     console.log(this.id);
     this.taskService.GetTasks().subscribe(obj => {
       this.task = obj.filter(c => c.id == this.id);
       console.log(this.task);
-    });
+    this.name=this.task[0].name;
+    this.creation_date=this.task[0].creation_date;
+    this.due_date=this.task[0].due_date;
+    this.physical_progress=this.task[0].physical_progress;
+    this.obj_status=this.task[0].obj_status;
+    this.description=this.task[0].description;
+    this.tags=this.task[0].tags; }
+    );
   }
 
 }
